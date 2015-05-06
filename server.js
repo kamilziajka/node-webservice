304564c5665d01db4b1c6605df8e972418bbec91
kazet15 <kazet15@gmail.com>
2015-05-06 20:00:48 +0200

    results filter

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 var soap = require('soap-server');
+var util = require('util');
 
 var ClientService = function () {
     this.clients = [];
 };
 
 ClientService.prototype.addClient = function (name) {
+    console.log(util.format('adding client, name: [%s].', name));
+
     this.clients.push(name);
-    return 'added client named ' + name;
 };
 
-ClientService.prototype.getClients = function (args) {
-    return this.clients.length;
+ClientService.prototype.getClients = function (name) {
+    console.log(util.format('getting clients list, filter: [%s].', name));
+
+    var filter = function (client) {
+        return client.indexOf(name) > -1;
+    };
+    return name === 'undefined' ? this.clients : this.clients.filter(filter);
 };
 
 var server = new soap.SoapServer();
@@ -18,9 +25,9 @@ var service = server.addService('ClientService', new ClientService());
 
 var addClientMethod = service.getOperation('addClient');
 addClientMethod.setInputType('name', { type: 'string' });
-addClientMethod.setOutputType('string');
 
 var getClientsMethod = service.getOperation('getClients');
-getClientsMethod.setOutputType('number');
+getClientsMethod.setInputType('name', { type: 'string' });
+getClientsMethod.setOutputType('string[]');
 
 server.listen(1337, '127.0.0.1');
